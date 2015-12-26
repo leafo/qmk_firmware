5b4364622d1842fcf63e981b2df219cdd8236b64
leaf corcoran <leafot@gmail.com>
2015-12-26 13:27:07 -0800

    use some missing keys

--- keyboard/ergodox_ez/keymaps/keymap_leaf.c
+++ keyboard/ergodox_ez/keymaps/keymap_leaf.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   =    |   1  |   2  |   3  |   4  |   5  | MO(1)|           | MO(1)|   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | tab    |   Q  |   W  |   E  |   R  |   T  | ctrl |           | tab  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | del    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------|lshift|           |rshift|------+------+------+------+------+--------|
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   MO(1),
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_TRNS,
+        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LCTL,
         KC_DELT,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LSHIFT,
         KC_TRNS,        KC_GRAVE,     KC_PAUS,KC_LEFT,KC_RGHT,
@@ -43,12 +43,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
         // right hand
-             MO(1),     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,     KC_MINS,
-             KC_TRNS,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     KC_BSLS,
+             MO(1),       KC_6,   KC_7,   KC_8,   KC_9,   KC_0,     KC_MINS,
+             KC_TAB,      KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,  KC_QUOT,
              KC_RSHIFT,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,  KC_RSFT,
                                   KC_DOWN,KC_UP,  KC_LBRC,KC_RBRC,  KC_TRNS,
-             KC_LGUI,KC_RCTRL,
+             KC_LGUI,KC_RCTL,
              KC_PGUP,
              KC_PGDN,KC_ENT, KC_SPC
     ),
